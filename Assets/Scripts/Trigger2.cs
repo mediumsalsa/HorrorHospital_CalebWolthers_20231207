fb7aa16879b9cdee2cc71096fe598d6824be04a2
Caleb WOlthers <w0473807@nscc.ca>
2023-12-06 12:04:38 -0400

    added

---
+++ Assets/Scripts/Trigger2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger2 : MonoBehaviour
+{
+
+
+    [SerializeField] UnityEvent onTriggerEnter;
+
+    [SerializeField] UnityEvent onTriggerExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        onTriggerEnter.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        onTriggerExit.Invoke();
+    }
+
+
+}
